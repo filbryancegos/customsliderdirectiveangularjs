228282de740c0b5667499d107358c7d059fc9474
Fil Bryan <filbryancegos@gmail.com>
2019-07-28 13:26:16 +0800

    custom slider directive

---
+++ main.js
@@ -0,0 +1,108 @@
+var app = angular.module('mySlider', []);
+app.controller('myCtrl', function($scope,$timeout,$interval,$http) {
+		$scope.photos = [
+			{
+				url: 'assets/images/image1.jpg',
+				content: 'the quick brown fox jumpover the lazy dog'
+			},
+			{
+				url: 'assets/images/image2.jpg',
+				content: 'the quick brown fox jumpover the lazy dog'
+			},
+			{
+				url: 'assets/images/image3.jpg',
+				content: 'the quick brown fox jumpover the lazy dog'
+            },
+            {
+				url: 'assets/images/image1.jpg',
+				content: 'the quick brown fox jumpover the lazy dog'
+            },
+            {
+				url: 'assets/images/image2.jpg',
+				content: 'the quick brown fox jumpover the lazy dog'
+            },
+            {
+				url: 'assets/images/image3.jpg',
+				content: 'the quick brown fox jumpover the lazy dog'
+            },
+            {
+				url: 'assets/images/image1.jpg',
+				content: 'the quick brown fox jumpover the lazy dog'
+            },
+            {
+				url: 'assets/images/image2.jpg',
+				content: 'the quick brown fox jumpover the lazy dog'
+            },
+            {
+				url: 'assets/images/image3.jpg',
+				content: 'the quick brown fox jumpover the lazy dog'
+			}
+
+		] 		
+});
+
+app.directive('photoInfo', function() {
+	return {
+		restrict: 'AE',
+        replace: true,
+        scope: {
+            photos: '='
+        },
+		link: function(scope,element,attrs) {
+            var photolength = scope.photos.length;
+            var sliderWrapper = photolength * 800;
+            var sliderWrappertotal = sliderWrapper + "px";
+
+            scope.sliderParent = sliderWrappertotal;
+            scope.count = 0;
+            scope.currentIndex = 0
+
+            scope.nextBol = true;
+            scope.prevBol = true;
+            scope.buttondisablednext = false; 
+            scope.buttondisabledprev = true; 
+            scope.next = function(){
+				if (scope.currentIndex < scope.photos.length -1) {
+                    scope.currentIndex++;
+                    scope.count = scope.count + -800;
+				} else {
+                    scope.currentIndex = 0;
+                    scope.count = 0;
+                }
+                var countwidht = scope.count + "px"
+                scope.slidewidth = 'translate('+ countwidht +')';
+                if (scope.currentIndex == scope.photos.length -1) {
+                    scope.buttondisablednext = true;
+                } else {
+                    scope.prevBol = true;
+                    scope.buttondisabledprev = false; 
+                }
+            };
+            
+            scope.prev = function(){
+				if (scope.currentIndex > 0) {
+                    scope.currentIndex--;
+                    scope.count = scope.count - -800;
+				} else {
+                    scope.photos.length;
+                    scope.count = 0;
+                }
+                var countwidht = scope.count + "px"
+                scope.slidewidth = 'translate('+ countwidht +')';
+
+                if (scope.currentIndex == 0) {
+                    scope.buttondisabledprev = true;
+                } else {
+                    scope.buttondisablednext = false; 
+                }
+			};
+        },
+        templateUrl:'template/sliderurl.html'
+	}
+})
+
+
+  
+	
+  
+
